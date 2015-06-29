3d61665d2f43ed39f87174ac3667f9f3daf59514
fumiko <fumi811bbc@gmail.com>
2015-06-30 00:16:58 +0900

    演算子まで

--- hello.c
+++ hello.c
@@ -1,6 +1,16 @@
 #include<stdio.h>
+/*コンパイルの仕方*/
+//gcc -o(アウトプット) hello(変換後の新しいものの名前) hello.c(変換するものの名前)
+//./hello(変換したファイルをコンパイル)
 
-int main(){
-	printf("Hello,World\n");
+int main(void){
+	//char(1文字) %c
+
+	int x = 10;
+	float y = 5.2;
+	char c = 'A';
+
+	printf("x = %d, y = %f, c = %c \n",x,y,c);
 	return 0;
+	
 }
