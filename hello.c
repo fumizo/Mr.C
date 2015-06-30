473983b3e63ee1b7727f3dc87fd671a98c2d5764
fumiko <fumi811bbc@gmail.com>
2015-07-01 00:41:24 +0900

    配列.文字列などなど

--- hello.c
+++ hello.c
@@ -4,32 +4,78 @@
 //./hello(変換したファイルをコンパイル)
 //⌘+Cでとめられる
 
-//プロトタイプ宣言
 
+//文字列は char型(一文字)の配列 (※終端は「\0」)
+
+void abc(void){
+	//char s[] = {'a','b','c','\0'};
+	char s[] = "abc";
+	//char s[4] = "abc"; //終端の０も含むから四個
+	printf("%c\n",s[1] );
+}
+
+int main(void)
+{
+	abc();
+
+	int sales[3];
+
+	sales[0] = 200;
+	sales[1] = 400;
+	sales[2] = 300;
+
+	int sales2[3] = {200, 400, 300};
+	//これでいっぺんにいれられる
+	//[3]のところを{}の中の値の個数に揃えたいときは省略して良い
+
+	printf("%d\n",sales[0] );
+
+	return 0;
+}
+
+
+//プロトタイプ宣言
 float getMax(float a, float b);
 //返り値の型、関数名、引数になにを使うか(関数の一行目)
 
+
+/*三項演算子		
+	返り値 = (条件) ? A :B ;
+	条件が真であればAを、そうでなければBを
+*/
 float getMax(float a, float b){
+	/*
 	if (a >= b)
 	{
 		return a;
 	} else {
 		return b;
 	}
+	*/
+	//三項演算子	
+	return (a >= b) ? a : b;
 }
 
 void sayHi(void){
 	printf("hi!\n");
+	//引数に型がない場合の関数
 }
 
+void f(void){
+	int a = 0;
+	a++;
+	printf("a:%d\n",a );
+}
 
-int main(void){
+int __main(void){
+
+	f();
 
 	float result;
 	result = getMax(2.3 , 5.2);
 	printf("%f\n", result);
 
-	sayHi();
+	// sayHi();
 
 
 	//char(1文字) %c
